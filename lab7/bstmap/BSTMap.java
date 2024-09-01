1a8b4cbeedbf571334d4947a8298f505934a41eb
Zane_Liao <lzq666amn@gmail.com>
2024-09-01 14:06:40 +0800

    14:06

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,52 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K, V> implements Map61B<K, V> {
+
+    @Override
+    public void clear() {
+        
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
