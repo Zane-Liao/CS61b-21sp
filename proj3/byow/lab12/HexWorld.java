1f6e47b2bcaf574707033160f1f5dbc75bab348b
Zane_Liao <lzq666amn@gmail.com>
2024-10-07 23:02:59 +0800

    23:02

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,43 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    private int s;
+
+    public HexWorld() {
+
+    }
+
+    public void addHexagon(int s) {
+
+    }
+
+    private void upperDrawHexagon(int s) {
+        for (int i = 0; i < s - 1; i++) {
+            System.out.println(' ');
+        }
+        for () {
+
+        }
+    }
+
+    private void middleDrawHexagon(int s) {
+        for (int i = 0; i < s; i++) {
+            TETile wall = Tileset.WALL;
+        }
+    }
+
+    private void downDrawHexagon(int s) {
+
+    }
+
+    private void rowHexagon(int s) {
+
+        if ((s & 1) == 1) {
+
+        }
+        else {
+
+        }
+    }
 }
